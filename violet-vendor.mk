ea74f9f76e13be513a2350e162300ef65814101d
Wang Han <wanghan1995315@gmail.com>
2019-08-10 11:17:36 +0800

    violet: Import SVI configuration
    Change-Id: I60a9d5978230f3cc3af674acac300a3ac4a4b54c

--- violet-vendor.mk
+++ violet-vendor.mk
@@ -18,6 +18,7 @@
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/violet/proprietary/bin/dpmd:system/bin/dpmd \
     vendor/xiaomi/violet/proprietary/bin/wfdservice:system/bin/wfdservice \
+    vendor/xiaomi/violet/proprietary/etc/display/SVIConfig.xml:system/etc/display/SVIConfig.xml \
     vendor/xiaomi/violet/proprietary/etc/dpm/dpm.conf:system/etc/dpm/dpm.conf \
     vendor/xiaomi/violet/proprietary/etc/init/dpmd.rc:system/etc/init/dpmd.rc \
     vendor/xiaomi/violet/proprietary/etc/init/wfdservice.rc:system/etc/init/wfdservice.rc \
